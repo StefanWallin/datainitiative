715255f81dcca16a97808841e0762502f5183fd2
Stefan Wallin <stefan.w@festiz.com>
2014-02-15 22:31:13 +0100

    Initial rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datainitiative::Application.config.secret_key_base = '57f4a620cf853b4cc9fb190949cb4ff79ae2ec10d1c548b679089f61d251a7a32df67c3fdd1683eb84bc3749dbb4c828d8e861819365c8cba534d15b76f21067'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Datainitiative::Application.config.session_store :cookie_store, key: '_datainitiative_session'
